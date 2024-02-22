ed0b4d3c48f6f42c05c74b4d3442be6d9f6016dc
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2024-02-22 16:37:36 +0300

    Completed Assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,36 @@
-#!/usr/bin/env python3
-
+# lib/book.py
 class Book:
-    pass
-    
+    def __init__(self, title, author, pages):
+        self._title = title
+        self._author = author
+        self._pages = pages
         
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str) or len(title) < 1 or len(title) > 100:
+            raise ValueError("Title must be a string with a length between 1 and 100 characters")
+        self._title = title
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, str) or len(author) < 1 or len(author) > 100:
+            raise ValueError("Author must be a string with a length between 1 and 100 characters")
+        self._author = author
+    
+    @property
+    def pages(self):
+        return self._pages
+    
+    @pages.setter
+    def pages(self, pages):
+        if not isinstance(pages, int) or pages <= 0:
+            raise ValueError("Number of pages must be an integer greater than 0")
+        self._pages = pages
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
-#!/usr/bin/env python3
-
+# lib/shoe.py
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self._brand = brand
+        self._size = size
+        self._color = color
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if not isinstance(brand, str) or len(brand) < 1 or len(brand) > 100:
+            raise ValueError("Brand must be a string with a length between 1 and 100 characters")
+        self._brand = brand
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, float) or size < 0 or size > 30:
+            raise ValueError("Size must be a float between 0 and 30")
+        self._size = size
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        if not isinstance(color, str) or len(color) < 1 or len(color) > 100:
+            raise ValueError("Color must be a string with a length between 1 and 100 characters")
+        self._color = color
